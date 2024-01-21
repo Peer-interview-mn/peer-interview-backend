d6b69e566ea830c9347eafe26ae2afc14ccbb063
Ds21saikhnaa <dashmandalsaikhanbileg@gmail.com>
2024-01-21 14:52:29 +0800

    protected the file upload

--- src/file-upload/file-upload.controller.ts
+++ src/file-upload/file-upload.controller.ts
@@ -3,17 +3,21 @@ import {
   Post,
   UseInterceptors,
   UploadedFile,
+  UseGuards,
 } from '@nestjs/common';
 import { FileUploadService } from './file-upload.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { AuthGuard } from '@nestjs/passport';
 
 @ApiTags('file-upload')
 @Controller('file-upload')
 export class FileUploadController {
   constructor(private readonly fileUploadService: FileUploadService) {}
 
+  @ApiBearerAuth()
   @Post()
+  @UseGuards(AuthGuard('jwt'))
   @UseInterceptors(FileInterceptor('file'))
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
     return await this.fileUploadService.uploadFile(file);
