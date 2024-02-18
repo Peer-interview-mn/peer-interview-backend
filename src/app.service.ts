1cb5d0319a409edda0f2fe83e2adc5e50099d7d8
Ds21saikhnaa <dashmandalsaikhanbileg@gmail.com>
2024-02-19 00:11:10 +0800

    cancel request

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'peer interview api';
+    return 'peer interview api...';
   }
 }
